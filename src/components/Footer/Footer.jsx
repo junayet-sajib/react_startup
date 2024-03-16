0d5262201259ee8f0a5d5ba35e554a95d00dbf40
Junayet Sajib <junayet3@gmail.com>
2024-03-16 15:29:40 +0600

    Added PropTypes

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+
+import PropTypes from 'prop-types';
+
+const Footer = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+Footer.propTypes ={
+
+}
+export default Footer;
